59b84668d01a09879afc0239a771b0b4c55ec382
Ramon <ramon.santoscosta@yahoo.com>
2022-12-13 14:58:26 -0300

    CORS config to allow all client connections

--- src/main/java/br/com/javamoon/foro11apiauth/core/web/CorsConfig.java
+++ src/main/java/br/com/javamoon/foro11apiauth/core/web/CorsConfig.java
@@ -11,21 +11,22 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class WebConfig{
+public class CorsConfig{
 
     @Bean
     public FilterRegistrationBean<CorsFilter> customCorsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        //config.setAllowCredentials(true);
+        config.setAllowCredentials(true);
         config.setAllowedMethods(Collections.singletonList("*"));
         config.setAllowedHeaders(Collections.singletonList("*"));
-        config.addAllowedOrigin("*");
-        //config.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/oauth2/token", config);
+        
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        
         return bean;
     }
 }
